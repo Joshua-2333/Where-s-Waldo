d5388e992f9c15547ed8f88f5155de7bba8ddb67
Joshua-2333 <jaflores.2333@gmail.com>
2026-01-25 04:45:44 -0600

    config update for Render deployment

--- client/config.js
+++ client/config.js
@@ -1,2 +1,5 @@
 // client/config.js
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:3000"
+    : "https://wheres-waldo-backend-fe40.onrender.com";
